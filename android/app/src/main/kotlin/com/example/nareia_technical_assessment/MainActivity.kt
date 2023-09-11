9823d7e473361a52f27003c1f3b3c3d816a9d6f9
Joao <joao_giacometti@hotmail.com>
2023-09-11 11:43:28 -0300

    feat: Project start

---
+++ android/app/src/main/kotlin/com/example/nareia_technical_assessment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nareia_technical_assessment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(Injector i) {}
+
+  @override
+  void routes(RouteManager r) {}
+}
